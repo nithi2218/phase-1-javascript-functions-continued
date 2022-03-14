cfbc82f3a4e99653d479352d142ca2728666602b
Nithyadevi <nithi2218@gmail.com>
2022-03-14 16:01:14 +1100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function saturdayFun(name="roller-skate") {
+        return `This Saturday, I want to ${name}!`;
+}
+
+
+function mondayWork(name = "go to the office") {
+    return `This Monday, I will ${name}.`;
+}
+
+function wrapAdjective(star="*") {
+    return function innerFunc(doubleLine="||") {
+         return `You are ${star}${doubleLine}${star}!`
+     }
+ }
+
+
+     
